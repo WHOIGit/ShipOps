c741307abdaeaafcd1594201a8261c6fd71b4970
Paul Mena <pmena@whoi.edu>
2025-05-28 19:35:42 +0000

    New listeners and YAML files with log name bugfixes

--- Atlantis/OpenRVDAS/listeners/start_listeners.sh
+++ Atlantis/OpenRVDAS/listeners/start_listeners.sh
@@ -2,16 +2,16 @@
 
 # Serial input
 echo "starting CNAV"
-./stream_cnav_serial.sh 2>/dev/null &
+./stream_cnav_serial_filewriter.sh 2>/dev/null &
 sleep 2
 echo "starting GYRO1"
-./stream_gyro1_serial.sh 2>/dev/null &
+./stream_gyro1_serial_filewriter.sh 2>/dev/null &
 sleep 2
 echo "starting SBE48"
 python ./stream_sbe48_serial.py 2>/dev/null &
 sleep 2
 echo "starting SBE45"
-./stream_sbe45_serial.sh 2>/dev/null &
+./stream_sbe45_serial_filewriter.sh 2>/dev/null &
 sleep 2
 echo "starting the RS485 loop"
 ./stream_485_serial_notransform.sh 2>/dev/null &
--- Atlantis/OpenRVDAS/listeners/stop_listeners.sh
+++ Atlantis/OpenRVDAS/listeners/stop_listeners.sh
@@ -11,6 +11,6 @@ done
 # Kill the RS485 post-processor(s)
 for i in `ps -ef | grep atlantis_rs485_udp_processor.py | grep -v grep | awk '{print $2}'`
 do
-    echo "killing pid ${i}"
-    kill -9 ${i}
+   echo "killing pid ${i}"
+   kill -9 ${i}
 done
